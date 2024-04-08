de368f43e472c037fa8a19a46045f4ed898ea2ab
Your Name <you@example.com>
2024-04-07 23:06:15 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = pygame.Rect((300, 250, 50, 50))
+
+text_font = pygame.font.SysFont("Arial", 30)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+run = True
+
+while run:
+
+    screen.fill((0, 0, 0))
+
+    draw_text("Hello World", text_font, (120, 0, 0), 220, 150)
+
+    pygame.draw.rect(screen, (255, 0, 0), player)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()

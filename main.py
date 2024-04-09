147ed7ffae52dfa8518c9ef522a0bf2900ecf246
Your Name <you@example.com>
2024-04-08 22:48:42 -0300

    adicionei texto ao bloco e um input que printa na tela o que escrevi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -7,17 +8,21 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+pygame.display.set_caption("Text Input Example")
+
 player = pygame.Rect((300, 250, 50, 50))
 
-text_font = pygame.font.SysFont("Arial", 50)
+text_font = pygame.font.SysFont("Arial", 20)
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
 
+
 black = (0, 0, 0)
 blue = (0, 0, 255)
+white = (255, 255, 255)
 
 block_width = 50
 block_height = 20
@@ -27,7 +32,13 @@ block_y = 0
 
 block_speed = 1
 
+font = pygame.font.SysFont(None, 50)
+
+text = "Hello World"
+
 clock = pygame.time.Clock()
+
+input_text = ""
 
 run = True
 
@@ -35,7 +46,7 @@ while run:
 
     screen.fill((0, 0, 0))
 
-    draw_text("Hello World", text_font, (120, 0, 0), 20, 530)
+    #draw_text("Hello World", text_font, white, 20, 530)
 
     pygame.draw.rect(screen, (255, 0, 0), player)
 
@@ -44,6 +55,11 @@ while run:
         block_y = -block_height
 
     pygame.draw.rect(screen, blue, [block_x, block_y, block_width, block_height])
+
+    #text_surface = font.render(text, True, white)
+    text_x = block_x + (block_width - text_font.size(text)[0]) / 2
+    text_y = block_y + (block_height - text_font.size(text)[1]) / 2
+    draw_text(text, text_font, white, text_x, text_y)
 
     clock.tick(60)
 
@@ -60,8 +76,14 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                input_text = input_text[:-1]
+            else:
+                input_text += event.unicode
 
-
+    text_surface = font.render(input_text, True, white)
+    screen.blit(text_surface, (20, SCREEN_HEIGHT - text_surface.get_height() - 20))
 
     pygame.display.update()
 
